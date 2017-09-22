269acb0678d6443070553a0db107d3b753ef2b59
Dustin Diaz <polvero@gmail.com>
2017-09-22 14:46:43 -0700

    working demo

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
+var express = require('express')
 let pug = require('pug')
 let Prom = require('bluebird')
+var router = express.Router()
 
-let dependency = require('node-rollout')
 let rollout = null
 
-
-let view = (req, res, next) => {
-  res.status(200)
-    .send(pug.renderFile('./views/index.pug', res.locals))
+let utils = {
+  formatFlag: (name) => {
+    return name.replace(/_/g, ' ').replace(/\b(\w)/g, (_, letter) => {
+      return letter.toUpperCase()
+    })
+  }
 }
 
+router.get('/', populate, (req, res, next) => {
+  res.status(200)
+    .send(pug.renderFile(`${__dirname}/views/index.pug`, res.locals))
+})
+
+router.put('/', populate, (req, res, next) => {
+  let body = JSON.parse(req.body.data)
+  let update = {}
+  update[body.mod] = body.val
+  console.log(body.flag, update)
+  rollout.update(body.flag, update)
+  .finally(() => {
+    return res.status(201).json({
+      ok: true
+    })
+  })
+})
+
+function populate(req, res, next) {
+  res.locals.utils = utils
+  let flags = res.locals.rollout_flags = rollout.flags()
+  let all = {}
+  Prom.all(res.locals.rollout_flags.map((flag) => {
+    return rollout.mods(flag)
+  }))
+  .then((mods) => {
+    mods.forEach((mod, i) => {
+      all[flags[i]] = mod
+    })
+    res.locals.all_mods = all
+  })
+  .finally(next)
+}
 
 module.exports.client = (client) => {
-  rollout = dependency(client)
-  return (req, res, next) => {
-    res.locals.rollout_flags = rollout.flags()
-    Prom.all(res.locals.rollout_flags.map((flag) => {
-      return rollout.mods(flag)
-    })
-    .then((mods) => {
-      console.log(mods)
-    })
-    .finally(next)
-  }
+  rollout = client
+  return router
 }
